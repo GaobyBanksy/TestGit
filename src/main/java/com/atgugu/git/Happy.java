ddf06ba12cdfb4cf6497226407ed122ccf45441d
eclipse_user <eclipse_user@atguigu.com>
2022-02-11 18:47:06 +1000

    third edit lefttttt

--- src/main/java/com/atgugu/git/Happy.java
+++ src/main/java/com/atgugu/git/Happy.java
@@ -1,7 +1,9 @@
 package com.atgugu.git;
 
 public class Happy {
-	public static void main(string[] args) {
-		System.out.println("Test ...");
+	
+	public static void main(String[] args) {
+		System.out.println("right ...");
 	}
+	
 }
